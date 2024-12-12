9e72ee8aca6aff85ed419dabaf4099c5446cb8b8
Stefan Philipp <stefan.philipp@secunet.com>
2024-12-12 17:51:57 +0100

    GT-366: Helper method added

--- org.globaltester.cryptoprovider.bc/src/org/globaltester/cryptoprovider/bc/CryptoProviderUtil.java
+++ org.globaltester.cryptoprovider.bc/src/org/globaltester/cryptoprovider/bc/CryptoProviderUtil.java
@@ -10,19 +10,28 @@ import org.bouncycastle.asn1.ASN1Integer;
 import org.bouncycastle.asn1.DERSequence;
 import org.bouncycastle.jcajce.provider.asymmetric.util.EC5Util;
 
-public class CryptoProviderUtil {
+public class CryptoProviderUtil
+{
 
 	public static final boolean WITH_COMPRESSED_ECPOINT_ENCODING = false;
 
-	private CryptoProviderUtil() {
+	private CryptoProviderUtil()
+	{
 		// hide implicit public constructor
 	}
 
-	public static org.bouncycastle.jce.spec.ECParameterSpec convertECParameterSpec(ECParameterSpec ecSpec) {
+	public static org.bouncycastle.jce.spec.ECParameterSpec convertECParameterSpec(ECParameterSpec ecSpec)
+	{
 		return EC5Util.convertSpec(ecSpec);
 	}
 
-	public static byte[] ecdsaSigToASN1(byte[] sigBuff) {
+	public static ECParameterSpec convertECParameterSpec(org.bouncycastle.math.ec.ECCurve curve, org.bouncycastle.jce.spec.ECParameterSpec ecSpec)
+	{
+		return EC5Util.convertSpec(EC5Util.convertCurve(curve, ecSpec.getSeed()), ecSpec);
+	}
+
+	public static byte[] ecdsaSigToASN1(byte[] sigBuff)
+	{
 		int sigHalfOfBuffLength = sigBuff.length / 2;
 		BigInteger r = new BigInteger(1, Arrays.copyOfRange(sigBuff, 0, sigHalfOfBuffLength));
 		BigInteger s = new BigInteger(1, Arrays.copyOfRange(sigBuff, sigHalfOfBuffLength, sigHalfOfBuffLength * 2));
@@ -31,7 +40,8 @@ public class CryptoProviderUtil {
 		vector.add(new ASN1Integer(s));
 		try {
 			return new DERSequence(vector).getEncoded();
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 		return null;
